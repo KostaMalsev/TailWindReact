10d37fdb4c1d830b65fcfd9ea6456276f1369ce1
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2021-11-21 13:55:10 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+//TODO
+console.log('starting react script')
+
+  const newDiv = document.createElement("div");
+
+  // and give it some content
+  const newContent = document.createTextNode("Hi there and greetings!");
+
+  // add the text node to the newly created div
+  newDiv.appendChild(newContent);
+
+  // add the newly created element and its content into the DOM
+  const currentDiv = document.getElementById("div1");
+  document.body.insertBefore(newDiv, currentDiv);
+  
+  
+  
+
+const Component = props => {
+		return (<div>My component!</div>);
+	};
+
+	const App = () => (
+		<div className="App">
+			<Component />
+		</div>
+	);
+
+	const root = document.querySelector('#root');
+	ReactDOM.render(<App />, root );
+
+);
